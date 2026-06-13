0a419be620ba37e337a8f67a77719735ef8c0df3
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-06-13 17:47:37 -0600

    fix: Corregir la asignación de la URL para obtener estudiantes

--- actions/estudiantes.ts
+++ actions/estudiantes.ts
@@ -1,7 +1,6 @@
 import { Estudiante } from '@/types/estudiantes.interface';
 
-const URL = `${process.env.GATEWAY_URL}`;
-
+const URL = process.env.NEXT_PUBLIC_GATEWAY_URL;
 export async function getAllStudents(): Promise<Estudiante[]> {
   const response = await fetch(`${URL}/estudiantes`, { cache: 'no-store' });
 
